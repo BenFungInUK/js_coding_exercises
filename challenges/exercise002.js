ad6cde598b069d62208a9eec77a4185fd7c7d7bc
BenFung <future28uk2021@gmail.com>
2022-02-07 15:19:57 +0000

    exercise 002 format doc

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,34 +1,34 @@
 function getFillings(sandwich) {
-  if (sandwich === undefined) throw new Error("ingredients is required");
-  return sandwich.fillings;
+	if (sandwich === undefined) throw new Error("ingredients is required");
+	return sandwich.fillings;
 }
 
 function isFromManchester(person) {
-  if (person === undefined) throw new Error("person is required");
-  return person.city === "Manchester" ? true : false;
+	if (person === undefined) throw new Error("person is required");
+	return person.city === "Manchester" ? true : false;
 }
 
 function getBusNumbers(people) {
-  if (people === undefined) throw new Error("people is required");
-  return Math.ceil(people / 40);
+	if (people === undefined) throw new Error("people is required");
+	return Math.ceil(people / 40);
 }
 
 function countSheep(arr) {
-  if (arr === undefined) throw new Error("arr is required");
-  return arr.filter((item) => item === "sheep").length;
+	if (arr === undefined) throw new Error("arr is required");
+	return arr.filter((item) => item === "sheep").length;
 }
 
 function hasMPostCode(person) {
-  if (person === undefined) throw new Error("person is required");
-  return person.address.city !== "Manchester" ? 
-					false : person.address.postCode[0] === 'M' ? 
-						true : false;
+	if (person === undefined) throw new Error("person is required");
+	return person.address.city !== "Manchester" ?
+		false : person.address.postCode[0] === 'M' ?
+			true : false;
 }
 
 module.exports = {
-  getFillings,
-  isFromManchester,
-  countSheep,
-  getBusNumbers,
-  hasMPostCode
+	getFillings,
+	isFromManchester,
+	countSheep,
+	getBusNumbers,
+	hasMPostCode
 };

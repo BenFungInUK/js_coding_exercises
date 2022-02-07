1cde58aae8a8ccfd326f89afff4f1559783021b6
BenFung <future28uk2021@gmail.com>
2022-02-07 18:52:12 +0000

    exercise 005

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -1,46 +1,69 @@
 const findNextNumber = (nums, n) => {
-  if (nums === undefined) throw new Error("nums is required");
-  if (n === undefined) throw new Error("n is required");
-  // Your code here!
+	if (nums === undefined) throw new Error("nums is required");
+	if (n === undefined) throw new Error("n is required");
+	var index = nums.indexOf(n);
+	return index === -1 ?
+		null : index + 1 > nums.length - 1 ?
+			null : nums[index + 1];
 };
 
 const count1sand0s = str => {
-  if (str === undefined) throw new Error("str is required");
-  // Your code here!
+	if (str === undefined) throw new Error("str is required");
+	return { "1": str.split("1").length - 1, "0": str.split("0").length - 1 };
 };
 
 const reverseNumber = n => {
-  if (n === undefined) throw new Error("n is required");
-  // Your code here!
+	if (n === undefined) throw new Error("n is required");
+	var reverseNum = 0;
+	while (n) {
+		reverseNum = (reverseNum * 10) + (n % 10);
+		n = Math.floor(n / 10);
+	}
+	return reverseNum;
 };
 
 const sumArrays = arrs => {
-  if (arrs === undefined) throw new Error("arrs is required");
-  // Your code here!
+	if (arrs === undefined) throw new Error("arrs is required");
+	return arrs.flat().reduce((previous, current) => previous + current);
 };
 
 const arrShift = arr => {
-  if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+	if (arr === undefined) throw new Error("arr is required");
+	return arr.map((item, index) => {
+		return index === 0 ?
+			arr[arr.length - 1] : index === arr.length - 1 ?
+				arr[0] : item;
+	})
 };
 
 const findNeedle = (haystack, searchTerm) => {
-  if (haystack === undefined) throw new Error("haystack is required");
-  if (searchTerm === undefined) throw new Error("searchTerm is required");
-  // Your code here!
+	if (haystack === undefined) throw new Error("haystack is required");
+	if (searchTerm === undefined) throw new Error("searchTerm is required");
+	var upperCaseSearch = searchTerm.toUpperCase();
+	for (var key in haystack) {
+		if (typeof haystack[key] === "string")
+			if (haystack[key].toUpperCase().includes(upperCaseSearch)) return true;
+	}
+	return false;
 };
 
 const getWordFrequencies = str => {
-  if (str === undefined) throw new Error("str is required");
-  // Your code here!
+	if (str === undefined) throw new Error("str is required");
+	var splitStr = str.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, "").split(" ");
+	var key = [...new Set(splitStr)];
+	var obj = {};
+	key.forEach((item) => {
+		obj[item] = splitStr.filter((strItem) => item.toUpperCase() === strItem.toUpperCase()).length;
+	});
+	return obj;
 };
 
 module.exports = {
-  findNextNumber,
-  count1sand0s,
-  reverseNumber,
-  sumArrays,
-  arrShift,
-  findNeedle,
-  getWordFrequencies
+	findNextNumber,
+	count1sand0s,
+	reverseNumber,
+	sumArrays,
+	arrShift,
+	findNeedle,
+	getWordFrequencies
 };

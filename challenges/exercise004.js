72bebff20eac900ad36d8141b11bbd5d5b78a956
BenFung <future28uk2021@gmail.com>
2022-02-07 17:19:19 +0000

    exercise 004 finalize

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,83 +1,47 @@
 function findSmallNums(nums) {
-	if (!nums) throw new Error("nums is required");
-	if (nums.length === 0) return [];
-	var arr = []
-	for (var i = 0; i < nums.length; i++) {
-		if (nums[i] < 1) arr.push(nums[i]);
-	}
-	return arr;
+  if (!nums) throw new Error("nums is required");
+  if (nums.length === 0) return [];
+  return nums.filter((item) => item < 1 );
 }
 
 function findNamesBeginningWith(names, char) {
-	if (!names) throw new Error("names is required");
-	if (!char) throw new Error("char is required");
-	var arr = []
-	for (var i = 0; i < names.length; i++) {
-		if (names[i][0] === char) arr.push(names[i]);
-	}
-	return arr;
+  if (!names) throw new Error("names is required");
+  if (!char) throw new Error("char is required");
+  return names.filter((item) => item[0] === char);
 }
 
 function findVerbs(words) {
-	if (!words) throw new Error("words is required");
-	var arr = []
-	for (var i = 0; i < words.length; i++) {
-		if (words[i].substring(0, 3) === "to ") arr.push(words[i]);
-	}
-	return arr;
+  if (!words) throw new Error("words is required");
+  return words.filter((item) => item.substring(0, 3) === "to ");
 }
 
 function getIntegers(nums) {
-	if (!nums) throw new Error("nums is required");
-	var arr = []
-	for (var i = 0; i < nums.length; i++) {
-		if (nums[i] * 10 % 10 === 0) arr.push(nums[i]);
-	}
-	return arr;
+  if (!nums) throw new Error("nums is required");
+  return nums.filter((item) => item * 10 % 10 === 0);
 }
 
 function getCities(users) {
-	if (!users) throw new Error("users is required");
-	var arr = []
-	for (var i = 0; i < users.length; i++) {
-		arr.push(users[i].data.city.displayName);
-	}
-	return arr;
+  if (!users) throw new Error("users is required");
+  return users.map((item) => item.data.city.displayName);
 }
 
 function getSquareRoots(nums) {
-	if (!nums) throw new Error("nums is required");
-	var arr = []
-	for (var i = 0; i < nums.length; i++) {
-		arr.push(Math.round(Math.sqrt(nums[i]) * 100) / 100);
-	}
-	return arr;
+  if (!nums) throw new Error("nums is required");
+  return nums.map((item) => Math.round(Math.sqrt(item) * 100) / 100);
 }
 
 function findSentencesContaining(sentences, str) {
-	if (!sentences) throw new Error("sentences is required");
-	if (!str) throw new Error("str is required");
-	var arr = [], upperCaseStr = str.toUpperCase()
-	for (var i = 0; i < sentences.length; i++) {
-		if (sentences[i].toUpperCase().includes(upperCaseStr))
-			arr.push(sentences[i]);
-	}
-	return arr;
+  if (!sentences) throw new Error("sentences is required");
+  if (!str) throw new Error("str is required");
+  var upperCaseStr = str.toUpperCase()
+  return sentences.filter((item) => item.toUpperCase().includes(upperCaseStr));
 }
 
 function getLongestSides(triangles) {
 	if (!triangles) throw new Error("triangles is required");
-	var arr = []
-	for (var i = 0; i < triangles.length; i++) {
-		var maxSide = 0;
-		for (var j = 0; j < triangles[i].length; j++) {
-			if (triangles[i][j] > maxSide) {
-				maxSide = triangles[i][j];
-			}
-		}
-		arr.push(maxSide);
-	}
-	return arr;
+	return triangles.map((item) => {
+		return item.reduce((previous, current) => previous < current ? current : previous);
+	});
 }
 
 module.exports = {
